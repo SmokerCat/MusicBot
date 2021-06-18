9181747e2e7e98c1788ed18c09e9dcea95693546
Smoker Cat <80395140+SmokerCat@users.noreply.github.com>
2021-06-18 18:00:52 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,66 +1,109 @@
-
+import logging
+from DaisyXMusic.modules.msg import Messages as tr
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from DaisyXMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
+logging.basicConfig(level=logging.INFO)
 
-from config import BOT_NAME as bn
-from helpers.filters import other_filters2
-
-
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    await message.reply_sticker("CAACAgUAAxkBAAImYGDMHTX4zOA9PWMPjokeT_woS2uzAAKqAgACgAxhVrW_bIls07PzHwQ")
-        
-    await message.reply_text(
-        f"""**Dear {message.from_user.first_name}!
-
-🌼 I am Aami Music Player. 🌼
-
-🥳 I can play music in your Telegram Group's Voice Chat😉
-
-
-⚜️You can make your own music bot just tap on deploy link 🔱
-
-
-Developed by ⚡ @Cat_of_TelegramX ⚡
-
-
-My commands - type  /help to get commands, which work in grp
-
-Thanks for using .
-
-Regrards [AAMI](https://t.me/Aami_song_bot)
-**
-        """,
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
+def _start(client, message):
+    client.send_message(message.chat.id,
+        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+        parse_mode="markdown",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🍁 REPO 🍁", url="https://t.me/Cat_Telegram_Project_Club")
-                  ],[
+                        "➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                [
                     InlineKeyboardButton(
-                        "🌼 Group", url="https://t.me/Cat_Telegram_Project_Club"
-                    ),
+                        "📲 Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
                     InlineKeyboardButton(
-                        "🌼 Channel", url="https://t.me/Cat_Telegram_Projects"
-                    )
-                ],[ 
+                        "💬 Support", url=f"https://t.me/{SUPPORT_GROUP}")
+                ],[
                     InlineKeyboardButton(
-                        "🦋 Add To Your Group 🦋", url="https://t.me/Aami_song_bot?startgroup=true"
-                    )]
+                        "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
+                ]
             ]
         ),
-     disable_web_page_preview=True
-    )
+        reply_to_message_id=message.message_id
+        )
 
 @Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
 async def gstart(_, message: Message):
-      await message.reply_text("""**⭐Aami MUSIC PLAYER IS ALWAYS ACTIVE!!⭐**""",
-      reply_markup=InlineKeyboardMarkup(
+    await message.reply_text(
+        f"""**🔴 {PROJECT_NAME} is online**""",
+        reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🌼 Channel", url="https://t.me/Cat_Telegram_Projects")
+                        "💬 Support Chat", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
                 ]
             ]
-        )
-   )
+        ),
+    )
+
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+def _help(client, message):
+    client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
+        parse_mode="markdown",
+        disable_web_page_preview=True,
+        disable_notification=True,
+        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_to_message_id = message.message_id
+    )
+
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
+
+@Client.on_callback_query(help_callback_filter)
+def help_answer(client, callback_query):
+    chat_id = callback_query.from_user.id
+    disable_web_page_preview=True
+    message_id = callback_query.message.message_id
+    msg = int(callback_query.data.split('+')[1])
+    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
+        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
+    )
+
+
+def map(pos):
+    if(pos==1):
+        button = [
+            [InlineKeyboardButton(text = '▶️', callback_data = "help+2")]
+        ]
+    elif(pos==len(tr.HELP_MSG)-1):
+        url = f"https://t.me/{SUPPORT_GROUP}"
+        button = [
+            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+            [InlineKeyboardButton(text = '📲 Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
+             InlineKeyboardButton(text = '💬 Support', url=f"https://t.me/{SUPPORT_GROUP}")],
+            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
+            [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
+        ]
+    else:
+        button = [
+            [
+                InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '▶️', callback_data = f"help+{pos+1}")
+            ],
+        ]
+    return button
+
+@Client.on_message(filters.command("help") & ~filters.private & ~filters.channel)
+async def ghelp(_, message: Message):
+    await message.reply_text(
+        f"""**🙋‍♀️ Hello there! I can play music in the voice chats of telegram groups & channels.**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🟡 Click here for help 🟡", url=f"https://t.me/{BOT_USERNAME}?start"
+                    )
+                ]
+            ]
+        ),
+    )
+
